d1f1b95ce994eec14bef826d0c8d0905ad16ae93
ComaszTyrulik <tomaszcyrulikdevelopment@gmail.com>
2020-12-20 19:08:31 +0100

    Added initializing script which creates virtual environment

---
+++ init.bat
@@ -0,0 +1,6 @@
+python -m venv .env
+ .\.env\Scripts\activate.bat^
+ & python -m pip install -r requirements.txt^
+ & python -m pip install --upgrade pip^
+ & python init_project.py^
+ & .\.env\Scripts\deactivate.bat
--- run_tests.bat
+++ run_tests.bat
@@ -1,1 +1,1 @@
-.\.env\Scripts\activate.bat & python run_tests.py %* & .\.env\Scripts\deactivate.bat
+@.\.env\Scripts\activate.bat & python run_tests.py %* & .\.env\Scripts\deactivate.bat
