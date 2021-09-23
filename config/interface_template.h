7f0ce016def2bdb032f33f48451b86076847eeb2
ComaszTyrulik <tomaszcyrulikdevelopment@gmail.com>
2021-09-23 12:40:43 +0200

    Updated interface template to use macro from CommonUtilities library. Added command to init.bat file which adds new conan remote

--- config/interface_template.h
+++ config/interface_template.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <Common/Helpers.h>
 
 namespace {namespace}
@@ -7,7 +6,7 @@ namespace {namespace}
     class {class_name}
     {{
     public:
-        SCE_IMPLEMENT_ADDITIONAL_CONSTRUCTORS_DELETED({class_name});
+        CT_IMPLEMENT_ADDITIONAL_CONSTRUCTORS_DELETED({class_name});
         {class_name}() = default;
         virtual ~{class_name}() = default;
     }};
