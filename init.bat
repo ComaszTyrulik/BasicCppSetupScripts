9140ab5eea0fd22614baf35ddfba4c2fa143032b
ComaszTyrulik <tomaszcyrulikdevelopment@gmail.com>
2021-09-23 15:44:10 +0200

    Removed adding the conancenter in initialization script

--- init.bat
+++ init.bat
@@ -4,6 +4,5 @@ python -m venv "%cwd%.env"
  "%cwd%.env\Scripts\activate.bat"^
  & python -m pip install -r "%cwd%requirements.txt"^
  & python -m pip install --upgrade pip^
- & conan remote add -i 0 conancenter https://center.conan.io
  & python "%cwd%init_project.py"^
  & "%cwd%.env\Scripts\deactivate.bat"
